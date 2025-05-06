daded59a1e6f02892e1d69f576507b9e1f09a1fc
takahc <108157287+takahc@users.noreply.github.com>
2025-05-06 09:59:52 +0900

    GhGH-test not work

--- GhGH-test/GhGH-test.cpp
+++ GhGH-test/GhGH-test.cpp
@@ -1,20 +1,136 @@
-﻿// GhGH-test.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include <dbghelp.h>
+#include <thread>
+#include <fstream>
 
-#include <iostream>
+extern "C" void enterFunction(void* returnAddress);
+extern "C" void exitFunction();
+void getFunctionNameFromAddress(void* address);
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// スレッドローカル変数で再帰防止フラグを管理
+thread_local bool isInPenter = false;
+thread_local bool isInPexit = false;
+thread_local int indent = 0;
+struct CallNode {
+    std::string functionName;
+    CallNode* parent;
+    std::vector<CallNode*> children;
+};
+
+//std::stack<CallNode*> callStack;
+//CallNode* root = nullptr;
+
+//std::ofstream debugLog("debug_log.txt", std::ios::out);
+
+extern "C" void __declspec(naked) _penter() {
+    __asm {
+        push eax
+        mov eax, [esp + 4] // リターンアドレスを取得
+        push eax
+    }
+
+    if (!isInPenter) {
+        isInPenter = true;
+        __asm {
+            call enterFunction
+        }
+        isInPenter = false;
+    }
+
+    __asm {
+        pop eax
+        pop eax
+        ret
+    }
 }
 
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+extern "C" void __declspec(naked) _pexit() {
+    if(!isInPexit) {
+		isInPexit = true;
+        __asm {
+            call exitFunction
+            ret
+        }
+        isInPexit = false;
+    }
+}
 
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
+void enterFunction(void* returnAddress) {
+    //debugLog << "enterFunction called with address: " << returnAddress << std::endl;
+    //std::string functionName = getFunctionNameFromAddress(returnAddress);
+
+    getFunctionNameFromAddress(returnAddress);
+
+    //CallNode* node = new CallNode{functionName, nullptr, {}};
+    //if (!callStack.empty()) {
+    //    node->parent = callStack.top();
+    //    callStack.top()->children.push_back(node);
+    //} else {
+    //    root = node;
+    //}
+    //callStack.push(node);
+    //printf("hello %x\n", returnAddress);
+}
+
+void exitFunction() {
+    if (!isInPexit) {
+        isInPexit = true;
+        indent--;
+        isInPexit = false;
+    }
+    //indent--;
+    //if (!callStack.empty()) {
+    //    callStack.pop();
+    //}
+}
+
+void getFunctionNameFromAddress(void* address) {
+    char buffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR)];
+    PSYMBOL_INFO symbol = (PSYMBOL_INFO)buffer;
+    symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+    symbol->MaxNameLen = MAX_SYM_NAME;
+
+    HANDLE process = GetCurrentProcess();
+    //SymInitialize(process, NULL, TRUE);
+
+    if (SymFromAddr(process, (DWORD64)address, 0, symbol)) {
+        for(int i=0; i<indent; i++)
+			printf("  ");
+        printf("get %s\n", symbol->Name);
+        //return symbol->Name;
+    }
+    return;
+}
+
+//void printCallTree(CallNode* node, int depth = 0) {
+//    if (!node) return;
+//    for (int i = 0; i < depth; ++i) std::cout << "  ";
+//    std::cout << node->functionName << std::endl;
+//    for (auto child : node->children) {
+//        printCallTree(child, depth + 1);
+//    }
+//}
+
+void testFunctionA() {
+    //std::cout << "Inside testFunctionA" << std::endl;
+    printf("Inside testFunctionA\n");
+}
+
+void testFunctionB() {
+    //std::cout << "Inside testFunctionB" << std::endl;
+    printf("Inside testFunctionB\n");
+    testFunctionA();
+}
+
+int main() {
+    HANDLE process = GetCurrentProcess();
+    SymInitialize(process, NULL, TRUE);
+
+    testFunctionB();
+    //printCallTree(root);
+    return 0;
+}
